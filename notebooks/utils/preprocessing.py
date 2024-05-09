2ed885758940ca7bbe273ddb9baa74fdee0a74df
Quoc Toan Tran <quoctoan09072002@gmail.com>
2024-05-09 16:49:58 +0700

    feat(preprocessing): update dictionary and preprocess logic

--- notebooks/utils/preprocessing.py
+++ notebooks/utils/preprocessing.py
@@ -15,29 +15,33 @@ def read_stop_word_dictionary(dictionary_path="utils/vietnamese-stopwords.txt"):
     return set(stopwords_dictionary.split("\n"))
 
 
+def read_acronym_dictionary(dictionary_path="utils/acronym_dictionary.json"):
+    with open(dictionary_path, "r", encoding="utf-8") as file:
+        acronym_dictionary = json.load(file)
+    return acronym_dictionary
+
+
 def lowercase_text(text: str):
     return text.lower()
 
 
 def remove_diacritic(text: str):
     nfkd_form = unicodedata.normalize("NFKD", text)
-    return "".join([c for c in nfkd_form if not unicodedata.combining(c)]).replace(
-        "đ", "d"
-    )
+    return "".join([c for c in nfkd_form if not unicodedata.combining(c)]).replace("đ", "d")
 
 
-def tokenize(text: str, tokenize_dictionary: dict):
-    sorted_items = sorted(
-        tokenize_dictionary.items(), key=lambda x: len(x[0]), reverse=True
-    )
+def replace_tokens(text: str, replacement_dictionary: dict):
+    pattern = r"(?<=\d)/(?=\d)"
+    text = re.sub(pattern, " kiệt ", text)
+    sorted_items = sorted(replacement_dictionary.items(), key=lambda x: len(x[0]), reverse=True)
     for original, token in sorted_items:
         pattern = re.compile(r"\b" + re.escape(original) + r"\b", re.IGNORECASE)
         text = pattern.sub(token, text)
     return text
 
 
-def combined_tokenize(text: str, tokenize_dictionary: dict):
-    tokenized_text = tokenize(text, tokenize_dictionary)
+def translate_sentences(text: str, dictionary: dict):
+    tokenized_text = replace_tokens(text, dictionary)
 
     list_token_tokenized_text = tokenized_text.split()
     token_diacritic_map = {}
@@ -48,7 +52,7 @@ def combined_tokenize(text: str, tokenize_dictionary: dict):
             no_diacritic_text += remove_diacritic(token) + " "
     no_diacritic_text = no_diacritic_text.strip()
 
-    no_diacritic_tokenized_text = tokenize(no_diacritic_text, tokenize_dictionary)
+    no_diacritic_tokenized_text = replace_tokens(no_diacritic_text, dictionary)
     for no_diacritic_token in no_diacritic_tokenized_text.split():
         if "_" in no_diacritic_token:
             start_of_word = float("inf")
@@ -57,15 +61,22 @@ def combined_tokenize(text: str, tokenize_dictionary: dict):
                 part_token = remove_diacritic(part_token)
                 start_of_word = min(start_of_word, token_diacritic_map.get(part_token))
                 end_of_word = max(end_of_word, token_diacritic_map.get(part_token)) + 1
-            list_token_tokenized_text[start_of_word:end_of_word] = [no_diacritic_token]
+
+            skip = False
+            for word in list_token_tokenized_text[start_of_word:end_of_word]:
+                if "_" in word:
+                    skip = True
+                    break
+
+            if not skip:
+                list_token_tokenized_text[start_of_word:end_of_word] = [no_diacritic_token]
 
     return " ".join([token for token in list_token_tokenized_text])
 
 
 def remove_stopwords(text: str, stopwords_dictionary: set):
     stopwords_regex = "|".join(
-        re.escape(stopword)
-        for stopword in sorted(stopwords_dictionary, key=len, reverse=True)
+        re.escape(stopword) for stopword in sorted(stopwords_dictionary, key=len, reverse=True)
     )
     text = re.sub(r"\b(?:" + stopwords_regex + r")(?:\W|$)", " ", text)
     text = text.strip()
@@ -73,12 +84,16 @@ def remove_stopwords(text: str, stopwords_dictionary: set):
     return text
 
 
-def ner_preprocessing(
+def preprocessing(
     text: str,
+    is_process_entity: bool = False,
     tokenize_dictionary: dict = read_tokenize_dictionary(),
     stopwords_dictionary: set = read_stop_word_dictionary(),
+    acronym_dictionary: dict = read_acronym_dictionary(),
 ):
     text = lowercase_text(text)
-    text = combined_tokenize(text, tokenize_dictionary)
-    text = remove_stopwords(text, stopwords_dictionary)
+    text = translate_sentences(text, acronym_dictionary)
+    if is_process_entity:
+        text = translate_sentences(text, tokenize_dictionary)
+        text = remove_stopwords(text, stopwords_dictionary)
     return text
